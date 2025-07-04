e1413e9bf051d4bb4c871a7a4c61f2e76b422b4b
zufichris <zufichris@gmail.com>
2025-07-04 09:47:41 +0400

    deploy

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -38,10 +38,13 @@ export default function Header() {
         <div className="mx-auto px-4 mb-3">
           <div className="flex items-center justify-between h-16">
             {/* Logo */}
-            <Link href="/" className="text-2xl font-bold text-black">
-              VendorVerse
+           <div className="flex-shrink-0">
+            <Link href="/">
+              <h1 className="text-2xl font-bold text-gray-900 tracking-tight">
+                LUXE
+              </h1>
             </Link>
-
+          </div> 
             {/* Desktop Navigation */}
             <nav className="hidden md:flex items-center space-x-8">
               <div className="relative group">
