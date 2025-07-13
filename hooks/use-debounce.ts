1b80072baed04df353f093749bb5b1f997540548
zufichris <zufichris@gmail.com>
2025-07-13 18:19:44 +0400

    Add new file use-debounce.ts

---
+++ hooks/use-debounce.ts
@@ -0,0 +1,13 @@
+
+"use client"
+import { useEffect, useState } from "react";
+
+export function useDebounce<T>(value: T, delay: number) {
+  const [debounced, setDebounced] = useState(value);
+  useEffect(() => {
+    const handler = setTimeout(() => setDebounced(value), delay);
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+  return debounced;
+}
+
