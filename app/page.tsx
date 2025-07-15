49292ea8cf150c7e8dc88c39756baea481a24446
zufichris <zufichris@gmail.com>
2025-07-16 01:20:03 +0400

    chore: update root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,5 @@
-import HeroSection from "@/components/home/hero-section";
-import FeaturedProducts from "@/components/home/featured-products";
-import CategoryShowcase from "@/components/home/category-showcase";
-import SpecialOffers from "@/components/home/special-offers";
-import TestimonialsSection from "@/components/home/testimonials-section";
-import BrandShowcase from "@/components/home/brand-showcase";
-import TrustIndicators from "@/components/home/trust-indicators";
-import BlogPreview from "@/components/home/blog-preview";
-import NewsletterSection from "@/components/home/newsletter-section";
+import HomeLoading from "./home/loading";
 
-export default function HomePage() {
-  return (
-    <main className="min-h-screen">
-      <HeroSection />
-      <FeaturedProducts />
-      {/* <SpecialOffers /> */}
-      <CategoryShowcase />
-      {/* <TestimonialsSection /> */}
-      {/* <BrandShowcase /> */}
-      {/* <BlogPreview /> */}
-      {/* <NewsletterSection /> */}
-      <TrustIndicators />
-    </main>
-  );
+export default function() {
+    return <HomeLoading />;
 }
