16a18ebcf316ddd690f19f263033ece39f80b244
zufichris <zufichris@gmail.com>
2025-07-16 01:20:03 +0400

    chore: update global loading page

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,13 +1,5 @@
-import { Loader2 } from "lucide-react"
+import HomeLoading from "./home/loading";
 
 export default function Loading() {
-  return (
-    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-      <div className="text-center">
-        <Loader2 className="h-12 w-12 animate-spin text-black mx-auto mb-4" />
-        <h2 className="text-xl font-semibold text-black mb-2">Loading...</h2>
-        <p className="text-gray-600">Please wait while we load the content</p>
-      </div>
-    </div>
-  )
+    return <HomeLoading />;
 }
