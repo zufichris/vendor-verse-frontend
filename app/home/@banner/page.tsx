9d26af6dd44c0995ee070c02ae24a262c25ac5e8
zufichris <zufichris@gmail.com>
2025-07-16 01:21:15 +0400

    chore(home): update banner page section

--- app/home/@banner/page.tsx
+++ app/home/@banner/page.tsx
@@ -1,5 +1,25 @@
-import HeroSection from "@/components/home/hero-section";
+import { Banners } from "./component";
+import { Api } from "@/utils";
+
+export interface Banner {
+    id: number;
+    title: string;
+    subtitle: string;
+    description: string;
+    image: string;
+    cta: string;
+    link: string;
+    color?: string;
+}
 
 export default async function() {
-    return <HeroSection />;
+    const res = await Api.get<Banner[]>("/product/banners");
+    if (!res.success) {
+        return (
+            <div className="h-20 mt-5  text-destructive flex flex-col items-center justify-center">
+                Failed to load banners
+            </div>
+        );
+    }
+    return <Banners banners={res.data} />;
 }
