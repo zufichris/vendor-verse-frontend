7c8eb82a0c60f9633ff158a9ef461de1907ef3dd
zufichris <zufichris@gmail.com>
2025-07-23 08:40:49 +0400

    updated default limit

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,7 +5,7 @@ import { Tabs, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
 
 
 export default async function HomePage() {
-  const res = await Api.get<QueryResponse<Product>>("/products/active");
+  const res = await Api.get<QueryResponse<Product>>("/products/shop?limit=12");
   if (!res.success) {
     return <div>Error Getting Products</div>;
   }
