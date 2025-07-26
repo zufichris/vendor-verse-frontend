eb6a6c855231add4ab227a702b50ac99132cba3b
zufichris <zufichris@gmail.com>
2025-07-26 15:18:57 +0400

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,30 +7,32 @@ import Footer from "@/components/layout/footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "VendorVerse - Premium E-commerce Experience",
-    description:
-        "Discover premium products with exceptional quality and style at VendorVerse.",
-    openGraph: {
-        images: [
-            {
-                url: "https://images.unsplash.com/photo-1664455340023-214c33a9d0bd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGVjb21tZXJjZXxlbnwwfHwwfHx8MA%3D%3D",
-            },
-        ],
-    },
+  title: "VendorVerse - Premium E-commerce Experience",
+  description:
+    "Discover premium products with exceptional quality and style at VendorVerse.",
+  openGraph: {
+    images: [
+      {
+        url: "https://images.unsplash.com/photo-1664455340023-214c33a9d0bd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGVjb21tZXJjZXxlbnwwfHwwfHx8MA%3D%3D",
+      },
+    ],
+  },
 };
 
 export default async function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>
-                <Header />
-                {children}
-                <Footer />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+
+        <main className="p-8">{children}</main>
+
+        <Footer />
+      </body>
+    </html>
+  );
 }
